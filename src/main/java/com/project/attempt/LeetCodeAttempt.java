a02d9003098be3974c46441daddb7ac5b56ca6c9
Emerson Millard <emillard.code@gmail.com>
2025-04-10 12:02:00 -0700

    Added challenge attempt.

--- src/main/java/com/project/attempt/LeetCodeAttempt.java
+++ src/main/java/com/project/attempt/LeetCodeAttempt.java
@@ -4,6 +4,66 @@ public class LeetCodeAttempt {
 
     public static void main(String[] args) {
 
+        int[] nums1 = { 4, 2, 3, 1 };
+        System.out.println(minimumTimeToRepairCars(nums1, 10));
+
+        int[] nums2 = { 5, 1, 8 };
+        System.out.println(minimumTimeToRepairCars(nums2, 6));
+
+    }
+    
+    public static int minimumTimeToRepairCars(int[] ranks, int cars) {
+
+        int[] assignedCars = new int[ranks.length];
+
+        return repairCars(ranks, cars, assignedCars);
+        
+    }
+
+    private static int repairCars(int[] ranks, int cars, int[] assignedCars) {
+
+        if (cars == 0) {
+
+            int totalTimeTaken = 0;
+
+            for (int i = 0; i < assignedCars.length; i++) {
+
+                int timeTaken = ranks[i] * assignedCars[i] * assignedCars[i];
+
+                if (timeTaken > totalTimeTaken) {
+                    totalTimeTaken = timeTaken;
+                }
+
+            }
+
+            return totalTimeTaken;
+
+        }
+
+        int[] assignedCarsNew = new int[assignedCars.length];
+
+        for (int i = 0; i < assignedCars.length; i++) {
+
+            assignedCarsNew[i] = assignedCars[i];
+
+        }
+
+        int minimumTime = Integer.MAX_VALUE;
+
+        for (int i = 0; i < assignedCars.length; i++) {
+
+            assignedCarsNew[i] = assignedCarsNew[i] + 1;
+            int timeTaken = repairCars(ranks, cars - 1, assignedCarsNew);
+            assignedCarsNew[i] = assignedCarsNew[i] - 1;
+
+            if (timeTaken < minimumTime) {
+                minimumTime = timeTaken;
+            }
+
+        }
+
+        return minimumTime;
+
     }
 
 }
